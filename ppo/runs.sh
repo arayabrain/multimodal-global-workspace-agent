f95779ee22644eaad0ff00ad5b5dcc30c679c998
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2022-09-02 16:47:38 +0900

    Comented out runs.sh; queued up PPO Perceiver where CA gets previous latents

--- ppo/runs.sh
+++ ppo/runs.sh
@@ -76,24 +76,25 @@ fi
     ### Pass previous latent with the audio and vision modalities
     # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Depth + Spectrogram SS1
     # export MASTER_PORT=8748 # Default port is 8738
-    for seed in 111 222 333; do
-        (sleep 1s && python ppo_av_nav.py \
-            --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0" \
-            --agent-type "perceiver-gwt-gwwm" \
-            --pgwt-depth 1 \
-            --pgwt-num-latents 8 \
-            --pgwt-latent-dim 64 \
-            --pgwt-cross-heads 1 \
-            --pgwt-latent-heads 4 \
-            --pgwt-use-sa False \
-            --pgwt-mod-embed 0 \
-            --pgwt-ca-prev-latents True \
-            --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-            --wandb --wandb-project ss-hab --wandb-entity dosssman \
-            --logdir-prefix $LOGDIR_PREFIX \
-            --seed $seed \
-        ) & # >& /dev/null &
-    done
+    # for seed in 111; do
+    # for seed in 222 333; do
+    #     (sleep 1s && python ppo_av_nav.py \
+    #         --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0" \
+    #         --agent-type "perceiver-gwt-gwwm" \
+    #         --pgwt-depth 1 \
+    #         --pgwt-num-latents 8 \
+    #         --pgwt-latent-dim 64 \
+    #         --pgwt-cross-heads 1 \
+    #         --pgwt-latent-heads 4 \
+    #         --pgwt-use-sa False \
+    #         --pgwt-mod-embed 0 \
+    #         --pgwt-ca-prev-latents True \
+    #         --config-path "env_configs/audiogoal_depth_nocont.yaml" \
+    #         --wandb --wandb-project ss-hab --wandb-entity dosssman \
+    #         --logdir-prefix $LOGDIR_PREFIX \
+    #         --seed $seed \
+    #     ) & # >& /dev/null &
+    # done
     # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Depth + Spectrogram SS1
 
 
