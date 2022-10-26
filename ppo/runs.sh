3c73c93f6127b75bdf8f9797ca788a6aa220262a
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2022-10-26 14:35:15 +0900

    Adding a few more experiments on the SAVi env; wokring on custom metrics for the RSA analysis

--- ppo/runs.sh
+++ ppo/runs.sh
@@ -13,20 +13,6 @@ if [ ! -d $WANDB_DIR ]; then
 fi
 
 # region: Custom PPO based on SS tasks
-    # region: Custom PPO; Depth + Spectrogram based task, SS1.0
-    # export MASTER_PORT=8738 # Default port is 8738
-    # for seed in 111; do
-    # for seed in 222 333; do
-    #     (sleep 1s && python ppo_av_nav.py \
-    #         --exp-name "ppo_av_nav__ss1_depth_spectro" \
-    #         --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-    #         --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #         --logdir-prefix $LOGDIR_PREFIX \
-    #         --seed $seed \
-    #     ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO; Depth + Spectrogram based task, SS1.0
-    
     # region: Custom PPO; RGB + Spectrogram based task, SS1.0
     # for seed in 111; do
     # for seed in 222 333; do
@@ -70,6 +56,7 @@ fi
     # done
     # endregion: Custom PPO; RGB + Spectrogram based task, SS1.0, Value Features Detach
 
+
     # region: Custom PPO; Blind + Spectrogram based task, SS1.0
     # for seed in 111 222 333; do
     #   (sleep 1s && python ppo_av_nav.py \
@@ -107,51 +94,7 @@ fi
     # done
     # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
 
-    ### Modality embeddings
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111; do
-    #     (sleep 1s && python ppo_av_nav.py \
-    #         --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_128" \
-    #         --agent-type "perceiver-gwt-gwwm" \
-    #         --pgwt-depth 1 \
-    #         --pgwt-num-latents 8 \
-    #         --pgwt-latent-dim 64 \
-    #         --pgwt-cross-heads 1 \
-    #         --pgwt-latent-heads 4 \
-    #         --pgwt-use-sa False \
-    #         --pgwt-mod-embed 128 \
-    #         --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-    #         --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #         --logdir-prefix $LOGDIR_PREFIX \
-    #         --seed $seed \
-    #     ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-
     ### Pass previous latent with the audio and vision modalities
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Depth + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111 222; do
-    # for seed in 333; do
-    #     (sleep 1s && python ppo_av_nav.py \
-    #         --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0_CAprevlats" \
-    #         --agent-type "perceiver-gwt-gwwm" \
-    #         --pgwt-depth 1 \
-    #         --pgwt-num-latents 8 \
-    #         --pgwt-latent-dim 64 \
-    #         --pgwt-cross-heads 1 \
-    #         --pgwt-latent-heads 4 \
-    #         --pgwt-use-sa False \
-    #         --pgwt-mod-embed 0 \
-    #         --pgwt-ca-prev-latents True \
-    #         --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-    #         --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #         --logdir-prefix $LOGDIR_PREFIX \
-    #         --seed $seed \
-    #     ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Depth + Spectrogram SS1
 
     # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
     # export MASTER_PORT=8748 # Default port is 8738
@@ -177,7 +120,7 @@ fi
     # done
     # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
 
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
+    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents Value Detached; RGB + Spectrogram SS1
     # export MASTER_PORT=8748 # Default port is 8738
     # for seed in 111 222; do
     #   (sleep 1s && python ppo_av_nav.py \
@@ -199,9 +142,9 @@ fi
     #       --seed $seed \
     #   ) & # >& /dev/null &
     # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
+    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents Value Detached; RGB + Spectrogram SS1
 
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
+    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Policy detached; RGB + Spectrogram SS1
     # export MASTER_PORT=8748 # Default port is 8738
     # for seed in 111 222; do
     #   (sleep 1s && python ppo_av_nav.py \
@@ -223,98 +166,7 @@ fi
     #       --seed $seed \
     #   ) & # >& /dev/null &
     # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
-
-    ## CA SA
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111; do
-    # for seed in 222 333; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #       --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_WithSA_CAskipq_CAnheads_1_SAnheads_4_modembed_0" \
-    #       --agent-type "perceiver-gwt-gwwm" \
-    #       --pgwt-depth 1 \
-    #       --pgwt-num-latents 8 \
-    #       --pgwt-latent-dim 64 \
-    #       --pgwt-cross-heads 1 \
-    #       --pgwt-latent-heads 4 \
-    #       --pgwt-use-sa True \
-    #       --pgwt-mod-embed 0 \
-    #       --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-    #       --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #       --logdir-prefix $LOGDIR_PREFIX \
-    #       --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; RGB + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111 222 333; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #       --exp-name "ppo_av_nav__ss1_rgb_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_WithSA_CAskipq_CAnheads_1_SAnheads_4_modembed_0" \
-    #       --agent-type "perceiver-gwt-gwwm" \
-    #       --pgwt-depth 1 \
-    #       --pgwt-num-latents 8 \
-    #       --pgwt-latent-dim 64 \
-    #       --pgwt-cross-heads 1 \
-    #       --pgwt-latent-heads 4 \
-    #       --pgwt-use-sa True \
-    #       --pgwt-mod-embed 0 \
-    #       --config-path "env_configs/audiogoal_rgb_nocont.yaml" \
-    #       --save-videos True \
-    #       --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #       --logdir-prefix $LOGDIR_PREFIX \
-    #       --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; RGB + Spectrogram SS1
-
-    ### Pass previous latent with the audio and vision modalities
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111; do
-    # for seed in 222 333; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #       --exp-name "ppo_av_nav__ss1_depth_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_WithSA_CAskipq_CAnheads_1_SAnheads_4_modembed_0_CAprevlats" \
-    #       --agent-type "perceiver-gwt-gwwm" \
-    #       --pgwt-depth 1 \
-    #       --pgwt-num-latents 8 \
-    #       --pgwt-latent-dim 64 \
-    #       --pgwt-cross-heads 1 \
-    #       --pgwt-latent-heads 4 \
-    #       --pgwt-use-sa True \
-    #       --pgwt-mod-embed 0 \
-    #       --pgwt-ca-prev-latents True \
-    #       --config-path "env_configs/audiogoal_depth_nocont.yaml" \
-    #       --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #       --logdir-prefix $LOGDIR_PREFIX \
-    #       --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
-
-    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Blind + Spectrogram SS1
-    # export MASTER_PORT=8748 # Default port is 8738
-    # for seed in 111 222 333; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #       --exp-name "ppo_av_nav__ss1_blind_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_WithSA_CAskipq_CAnheads_1_SAnheads_4_modembed_0_CAprevlats" \
-    #       --agent-type "perceiver-gwt-gwwm" \
-    #       --pgwt-depth 1 \
-    #       --pgwt-num-latents 8 \
-    #       --pgwt-latent-dim 64 \
-    #       --pgwt-cross-heads 1 \
-    #       --pgwt-latent-heads 4 \
-    #       --pgwt-use-sa True \
-    #       --pgwt-mod-embed 0 \
-    #       --pgwt-ca-prev-latents True \
-    #       --config-path "env_configs/audiogoal_blind_nocont.yaml" \
-    #       --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #       --logdir-prefix $LOGDIR_PREFIX \
-    #       --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Blind + Spectrogram SS1
+    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; Policy detached; RGB + Spectrogram SS1
 
     ### Various latent dimension combinations
     # region: Custom PPO + Perceiver GWT GWWM Basic Arch. WithSA Cross Heads 1 SA Heads 4 mod_emb 0; Depth + Spectrogram SS1
@@ -391,62 +243,40 @@ fi
 
     ### SAVi ###
 
-    # region: SAVi SS1: PPO GRU Default config
-    # for seed in 111; do
-    #     (sleep 1s && python ppo_av_nav.py \
-    #         --config-path "env_configs/savi/savi_ss1.yaml" \
-    #         --exp-name "ppo_savi_ss1__ppo_gru" \
-    #         --total-steps 10000000 \
-    #         --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #         --logdir-prefix $LOGDIR_PREFIX \
-    #         --seed $seed
-    #     ) & # >& /dev/null &
-    # done
-    # endregion: SAVi SS1: PPO GRU Default config
+    # region: Custom PPO; RGB + Spectrogram based task, SAVi SS1.0
+    for seed in 111; do
+      (sleep 1s && python ppo_av_nav.py \
+          --exp-name "ppo_savi__ss1_rgb_spectro" \
+          --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
+          --save-videos True \
+          --wandb --wandb-project ss-hab --wandb-entity dosssman \
+          --logdir-prefix $LOGDIR_PREFIX \
+          --seed $seed \
+      ) & # >& /dev/null &
+    done
+    # endregion: Custom PPO; RGB + Spectrogram based task, SAVi SS1.0
 
-    # region: SAVi SS1: PPO PGWT NoSA CAHeads 1 SAHeads 4 ModEmbed 0 CA WithPrevLatents
-    # for seed in 111; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #     --config-path "env_configs/savi/savi_ss1.yaml" \
-    #     --exp-name "ppo_savi_ss1__pgwt_gwwm__N_8_D_64_CA_CAPrevLatents_CAHeads_1_SAHeads_4" \
-    #     --total-steps 10000000 \
-    #     --agent-type "perceiver-gwt-gwwm" \
-    #     --pgwt-depth 1 \
-    #     --pgwt-num-latents 8 \
-    #     --pgwt-latent-dim 64 \
-    #     --pgwt-cross-heads 1 \
-    #     --pgwt-latent-heads 4 \
-    #     --pgwt-use-sa False \
-    #     --pgwt-mod-embed 0 \
-    #     --pgwt-ca-prev-latents True \
-    #     --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #     --logdir-prefix $LOGDIR_PREFIX \
-    #     --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: SAVi SS1: PPO PGWT NoSA CAHeads 1 SAHeads 4 ModEmbed 0 CA WithPrevLatents
-
-    # region: SAVi SS1: PPO PGWT CA SA CAHeads 1 SAHeads 4 ModEmbed 0 CA
-    # for seed in 111; do
-    #   (sleep 1s && python ppo_av_nav.py \
-    #     --config-path "env_configs/savi/savi_ss1.yaml" \
-    #     --exp-name "ppo_savi_ss1__pgwt_gwwm__N_8_D_64_CA_SA_CAHeads_1_SAHeads_4" \
-    #     --total-steps 10000000 \
-    #     --agent-type "perceiver-gwt-gwwm" \
-    #     --pgwt-depth 1 \
-    #     --pgwt-num-latents 8 \
-    #     --pgwt-latent-dim 64 \
-    #     --pgwt-cross-heads 1 \
-    #     --pgwt-latent-heads 4 \
-    #     --pgwt-use-sa True \
-    #     --pgwt-mod-embed 0 \
-    #     --pgwt-ca-prev-latents True \
-    #     --wandb --wandb-project ss-hab --wandb-entity dosssman \
-    #     --logdir-prefix $LOGDIR_PREFIX \
-    #     --seed $seed \
-    #   ) & # >& /dev/null &
-    # done
-    # endregion: SAVi SS1: PPO PGWT CA SA CAHeads 1 SAHeads 4 ModEmbed 0 CA WithPrevLatents
+    # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SAVi SS1
+    for seed in 111; do
+      (sleep 1s && python ppo_av_nav.py \
+          --exp-name "ppo_savi__ss1_rgb_spectro__pgwt_gwwm__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0_CAprevlats" \
+          --agent-type "perceiver-gwt-gwwm" \
+          --pgwt-depth 1 \
+          --pgwt-num-latents 8 \
+          --pgwt-latent-dim 64 \
+          --pgwt-cross-heads 1 \
+          --pgwt-latent-heads 4 \
+          --pgwt-use-sa False \
+          --pgwt-mod-embed 0 \
+          --pgwt-ca-prev-latents True \
+          --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
+          --save-videos True \
+          --wandb --wandb-project ss-hab --wandb-entity dosssman \
+          --logdir-prefix $LOGDIR_PREFIX \
+          --seed $seed \
+      ) & # >& /dev/null &
+    done
+    # endregion: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SAVi SS1
 
 # endregion: Custom PPO based on SS tasks
 
