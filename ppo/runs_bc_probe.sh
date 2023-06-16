31ea98937eebcf527caf61a336d97b4d73f67444
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2023-06-16 16:02:22 +0900

    Slight tweaks to PPO BC; adding RGB + Spectrogram experiment training for max

--- ppo/runs_bc_probe.sh
+++ ppo/runs_bc_probe.sh
@@ -19,77 +19,77 @@ echo "${LD_LIBRARY_PATH}"
 
 
 # region: Probing ppo_gru__random
-for seed in 111; do
-    TOTAL_STEPS=500000; N_EPOCHS=10;
-    (sleep 1s && python ppo_bc_probe_train.py \
-        --exp-name "ppo_gru__random__fixTB" \
-        --pretrained-model-name "ppo_gru__random" \
-        --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
-        --save-videos False \
-        --ent-coef 0 \
-        --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
-        --logdir-prefix $LOGDIR_PREFIX \
-        --total-steps $TOTAL_STEPS \
-        --n-epochs $N_EPOCHS \
-        --seed $seed \
-    ) & # >& /dev/null &
-done
+# for seed in 111; do
+#     TOTAL_STEPS=500000; N_EPOCHS=10;
+#     (sleep 1s && python ppo_bc_probe_train.py \
+#         --exp-name "ppo_gru__random__fixTB" \
+#         --pretrained-model-name "ppo_gru__random" \
+#         --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
+#         --save-videos False \
+#         --ent-coef 0 \
+#         --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
+#         --logdir-prefix $LOGDIR_PREFIX \
+#         --total-steps $TOTAL_STEPS \
+#         --n-epochs $N_EPOCHS \
+#         --seed $seed \
+#     ) & # >& /dev/null &
+# done
 # endregion: Probing ppo_gru__random
 
 # region: Probing ppo_pgwt__random
-for seed in 111; do
-    TOTAL_STEPS=500000; N_EPOCHS=10;
-    (sleep 1s && python ppo_bc_probe_train.py \
-        --exp-name "ppo_pgwt__random__fixTB" \
-        --pretrained-model-name "ppo_pgwt__random" \
-        --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
-        --save-videos False \
-        --ent-coef 0 \
-        --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
-        --logdir-prefix $LOGDIR_PREFIX \
-        --total-steps $TOTAL_STEPS \
-        --n-epochs $N_EPOCHS \
-        --seed $seed \
-    ) & # >& /dev/null &
-done
+# for seed in 111; do
+#     TOTAL_STEPS=500000; N_EPOCHS=10;
+#     (sleep 1s && python ppo_bc_probe_train.py \
+#         --exp-name "ppo_pgwt__random__fixTB" \
+#         --pretrained-model-name "ppo_pgwt__random" \
+#         --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
+#         --save-videos False \
+#         --ent-coef 0 \
+#         --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
+#         --logdir-prefix $LOGDIR_PREFIX \
+#         --total-steps $TOTAL_STEPS \
+#         --n-epochs $N_EPOCHS \
+#         --seed $seed \
+#     ) & # >& /dev/null &
+# done
 # endregion: Probing ppo_pgwt__random
 
 # region: Probing ppo_bc__rgbd_spectro__gru__SAVi
-for seed in 111; do
-    TOTAL_STEPS=500000; N_EPOCHS=10;
-    (sleep 1s && python ppo_bc_probe_train.py \
-        --exp-name "ppo_bc__rgbd_spectro__gru__SAVi__fixTB" \
-        --pretrained-model-name "ppo_bc__rgbd_spectro__gru__SAVi" \
-        --pretrained-model-path "/home/rousslan/random/rl/exp-logs/ss-hab-bc/ppo_bc__savi_ss1_rgbd_spectro__gru_seed_111__2023_06_10_16_05_39_999286.musashi/models/ppo_agent.4995001.ckpt.pth" \
-        --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
-        --save-videos False \
-        --ent-coef 0 \
-        --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
-        --logdir-prefix $LOGDIR_PREFIX \
-        --total-steps $TOTAL_STEPS \
-        --n-epochs $N_EPOCHS \
-        --seed $seed \
-    ) & # >& /dev/null &
-done
+# for seed in 111; do
+#     TOTAL_STEPS=500000; N_EPOCHS=10;
+#     (sleep 1s && python ppo_bc_probe_train.py \
+#         --exp-name "ppo_bc__rgbd_spectro__gru__SAVi__fixTB" \
+#         --pretrained-model-name "ppo_bc__rgbd_spectro__gru__SAVi" \
+#         --pretrained-model-path "/home/rousslan/random/rl/exp-logs/ss-hab-bc/ppo_bc__savi_ss1_rgbd_spectro__gru_seed_111__2023_06_10_16_05_39_999286.musashi/models/ppo_agent.4995001.ckpt.pth" \
+#         --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
+#         --save-videos False \
+#         --ent-coef 0 \
+#         --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
+#         --logdir-prefix $LOGDIR_PREFIX \
+#         --total-steps $TOTAL_STEPS \
+#         --n-epochs $N_EPOCHS \
+#         --seed $seed \
+#     ) & # >& /dev/null &
+# done
 # endregion: Probing ppo_bc__rgbd_spectro__gru__SAVi
 
 # region: Probing ppo_bc__rgbd_spectro__pgwt__SAVi
-for seed in 111; do
-    TOTAL_STEPS=500000; N_EPOCHS=10;
-    (sleep 1s && python ppo_bc_probe_train.py \
-        --exp-name "ppo_bc__rgbd_spectro__pgwt__SAVi__fixTB" \
-        --pretrained-model-name "ppo_bc__rgbd_spectro__pgwt__SAVi" \
-        --pretrained-model-path "/home/rousslan/random/rl/exp-logs/ss-hab-bc/ppo_bc__savi_ss1_rgbd__spectro__pgwt__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0_CAprevlats_seed_111__2023_06_10_16_05_37_098602.musashi/models/ppo_agent.4995001.ckpt.pth" \
-        --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
-        --save-videos False \
-        --ent-coef 0 \
-        --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
-        --logdir-prefix $LOGDIR_PREFIX \
-        --total-steps $TOTAL_STEPS \
-        --n-epochs $N_EPOCHS \
-        --seed $seed \
-    ) & # >& /dev/null &
-done
+# for seed in 111; do
+#     TOTAL_STEPS=500000; N_EPOCHS=10;
+#     (sleep 1s && python ppo_bc_probe_train.py \
+#         --exp-name "ppo_bc__rgbd_spectro__pgwt__SAVi__fixTB" \
+#         --pretrained-model-name "ppo_bc__rgbd_spectro__pgwt__SAVi" \
+#         --pretrained-model-path "/home/rousslan/random/rl/exp-logs/ss-hab-bc/ppo_bc__savi_ss1_rgbd__spectro__pgwt__dpth_1_nlats_8_latdim_64_noSA_CAnheads_1_SAnheads_4_modembed_0_CAprevlats_seed_111__2023_06_10_16_05_37_098602.musashi/models/ppo_agent.4995001.ckpt.pth" \
+#         --config-path "env_configs/savi/savi_ss1_rgbd_spectro.yaml" \
+#         --save-videos False \
+#         --ent-coef 0 \
+#         --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
+#         --logdir-prefix $LOGDIR_PREFIX \
+#         --total-steps $TOTAL_STEPS \
+#         --n-epochs $N_EPOCHS \
+#         --seed $seed \
+#     ) & # >& /dev/null &
+# done
 # endregion: Probing ppo_bc__rgbd_spectro__pgwt__SAVi
 
 # Clean up env vars
