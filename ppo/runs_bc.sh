012da1b35341ea9fc99964527e89d2f402205174
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2023-07-25 17:56:48 +0900

    Added support for custom GWT agent with BU only; running some preliminary experiments

--- ppo/runs_bc.sh
+++ ppo/runs_bc.sh
@@ -159,21 +159,21 @@ echo "${LD_LIBRARY_PATH}"
       # endregion: PPO GRU - BC with default hyparams
 
       # region: PPO GRU 2 - BC with default hyparams, with pose
-      for seed in 111 222; do
-        # export MASTER_PORT=8738 # Default port is 8738
-        export TOTAL_STEPS=10000000
-        (sleep 1s && python ppo_bc.py \
-          --exp-name "ppo_bc__savi_ss1_rgb_spectro__gru2" \
-          --agent-type "custom-gru" \
-          --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
-          --save-videos False \
-          --ent-coef 0 \
-          --wandb --wandb-project "ss-hab-bc" --wandb-entity dosssman \
-          --logdir-prefix $LOGDIR_PREFIX \
-          --total-steps $TOTAL_STEPS \
-          --seed $seed \
-        ) & # >& /dev/null &
-      done
+      # for seed in 111 222; do
+      #   # export MASTER_PORT=8738 # Default port is 8738
+      #   export TOTAL_STEPS=10000000
+      #   (sleep 1s && python ppo_bc.py \
+      #     --exp-name "ppo_bc__savi_ss1_rgb_spectro__gru2" \
+      #     --agent-type "custom-gru" \
+      #     --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
+      #     --save-videos False \
+      #     --ent-coef 0 \
+      #     --wandb --wandb-project "ss-hab-bc" --wandb-entity dosssman \
+      #     --logdir-prefix $LOGDIR_PREFIX \
+      #     --total-steps $TOTAL_STEPS \
+      #     --seed $seed \
+      #   ) & # >& /dev/null &
+      # done
       # endregion: PPO GRU2 - BC with default hyparams
 
       # region: PPO Custom GWT with GRU - BC with default hyparams
@@ -194,6 +194,24 @@ echo "${LD_LIBRARY_PATH}"
       #   ) & # >& /dev/null &
       # done
       # endregion: PPO Custom GWT with GRU - BC with default hyparams
+
+      # region: PPO Custom GWT with GRU BU - BC with default hyparams
+      # for seed in 111 222; do
+      #   # export MASTER_PORT=8738 # Default port is 8738
+      #   export TOTAL_STEPS=10000000
+      #   (sleep 1s && python ppo_bc.py \
+      #     --exp-name "ppo_bc__savi_ss1_rgb_spectro__gwt_bu" \
+      #     --agent-type "custom-gwt-bu" \
+      #     --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
+      #     --save-videos False \
+      #     --ent-coef 0 \
+      #     --wandb --wandb-project "ss-hab-bc" --wandb-entity dosssman \
+      #     --logdir-prefix $LOGDIR_PREFIX \
+      #     --total-steps $TOTAL_STEPS \
+      #     --seed $seed \
+      #   ) & # >& /dev/null &
+      # done
+      # endregion: PPO Custom GWT with GRU BU - BC with default hyparams
 
       # region: Custom PPO + Perceiver GWT GWWM Basic Arch. NoSA Cross Heads 1 SA Heads 4 mod_emb 0 CA Prev Latents; RGB + Spectrogram SS1
       # for seed in 111 222; do
