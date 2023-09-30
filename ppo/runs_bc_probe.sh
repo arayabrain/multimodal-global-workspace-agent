28d1b67583dbe67d8b151fbe38b4e009e7d6af5e
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2023-10-01 05:35:41 +0900

    Added linear probing runs for the PGWT varaint with 4 CA heads

--- ppo/runs_bc_probe.sh
+++ ppo/runs_bc_probe.sh
@@ -193,6 +193,27 @@ echo "${LD_LIBRARY_PATH}"
 # done
 # endregion: Probing ppo_bc__rgb_spectro__pgwt__SAVi (No depth) @10M steps, Linear probe with minibatch train
 
+# region: Probing ppo_bc__rgb_spectro__pgwt__SAVi with ca heads = 4 (No depth) @10M steps, Linear probe with minibatch train
+# for seed in 111; do
+#     TOTAL_STEPS=500000; N_EPOCHS=10;
+#     (sleep 1s && python ppo_bc_probe_train_mb.py \
+#         --exp-name "ppo_bc__rgb_spectro__pgwt__n_ca_heads_4__SAVi__9990001__n_mb_50" \
+#         --num-minibatches 50 \
+#         --pretrained-model-name "ppo_bc__rgb_spectro__pgwt__SAVi" \
+#         --agent-type "perceiver-gwt-gwwm" \
+#         --pgwt-cross-heads 4 \
+#         --pretrained-model-path "/home/rousslan/random/rl/exp-logs/ss-hab-bc/ppo_bc__savi_ss1_rgb__spectro__pgwt__dpth_1_nlats_8_latdim_64_noSA_CAnheads_4_SAnheads_4_modembed_0_CAprevlats_seed_111__2023_09_26_09_44_38_705854.musashi/models/ppo_agent.9990001.ckpt.pth" \
+#         --config-path "env_configs/savi/savi_ss1_rgb_spectro.yaml" \
+#         --save-videos False \
+#         --ent-coef 0 \
+#         --wandb --wandb-project "ss-hab-bc-probing" --wandb-entity dosssman \
+#         --logdir-prefix $LOGDIR_PREFIX \
+#         --total-steps $TOTAL_STEPS \
+#         --n-epochs $N_EPOCHS \
+#         --seed $seed \
+#     ) & # >& /dev/null &
+# done
+# endregion: Probing ppo_bc__rgb_spectro__pgwt__SAVi with ca heads = 4 (No depth) @10M steps, Linear probe with minibatch train
 
 ## RNN agents get inputs of shape T * B = 150 * 10, but the input for the probes is broken into batches of size 30, probe depth 2
 # region: Probing ppo_bc__rgb_spectro__gru__SAVi (No depth) @10M steps, Linear probe with minibatcdh train
