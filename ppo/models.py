8f085ba60d65c56e93afdd35450819b01a65ec8e
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2024-03-26 15:07:43 +0900

    Bug fix for visual and audio encoder features probing stacking the wrong dimension, leading to wrong probe loss in some cases

--- ppo/models.py
+++ ppo/models.py
@@ -9,8 +9,8 @@ import torch.nn as nn
 from ss_baselines.common.utils import CategoricalNet, Flatten
 
 GWTAGENT_DEFAULT_ANALYSIS_LAYER_NAMES = [
-  "visual_encoder",
-  "audio_encoder",
+  "visual_encoder.rnn",
+  "audio_encoder.rnn",
   "state_encoder"
 ]
 
@@ -806,12 +806,14 @@ class GRU_Actor(nn.Module):
             gw_list.append(gw)
 
         # TODO: consider dropping legacy encoder name support (YYY.cnn.7)
-        if "visual_encoder" in self.analysis_layers:
-            self._features["visual_encoder.rnn"] = th.cat(modality_features_list["visual"], dim=0)
-        if "audio_encoder" in self.analysis_layers:
-            self._features["audio_encoder.rnn"] = th.cat(modality_features_list["audio"], dim=0)
+        if "visual_encoder.rnn" in self.analysis_layers:
+            self._features["visual_encoder.rnn"] = \
+                th.stack(modality_features_list["visual"]).permute(1, 0, 2).reshape(B * T, -1)
+        if "audio_encoder.rnn" in self.analysis_layers:
+            self._features["audio_encoder.rnn"] = \
+                th.stack(modality_features_list["audio"]).permute(1, 0, 2).reshape(B * T, -1)
 
-        # Returns gw_list as B * T, GW_H, to mach "action_list" used for CE loss
+        # Returns gw_list as B * T, GW_H, to match "XXX_target_list" used for CE loss
         return th.stack(gw_list).permute(1, 0, 2).reshape(B * T, -1), \
                gw, modality_features # [B * T, H], [B, H], {k: [B, H]} for k in "visual", "audio"
 
